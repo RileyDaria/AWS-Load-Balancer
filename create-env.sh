803f24fd508fc0c0be7b70e6a5a800e81639f4cc
RileyDaria <driley3@hawk.iit.edu>
2016-10-09 23:42:01 -0500

    corrected description

--- create-env.sh
+++ create-env.sh
@@ -10,9 +10,9 @@ echo "Autoscaling Launch Configured"
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name itmo544-hw4 --launch-configuration itmo544-dr-hw4 --availability-zone us-west-2b --load-balancer-name itmo544-dr --max-size 5 --min-size 2 --desired-capacity 4
 echo "Autoscaling group created"
 
-#Attach instances to the load balancer
+#Attach balancer to the group
 aws autoscaling attach-load-balancers --load-balancer-names itmo544-dr --auto-scaling-group-name itmo544-hw4
-echo "Instances attached"
+echo "Balancer attached"
 
 
 
