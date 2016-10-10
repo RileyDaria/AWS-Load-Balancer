015d056ec10058224174124ac61b6dd3779f3c58
RileyDaria <driley3@hawk.iit.edu>
2016-10-09 23:45:22 -0500

    added comments

--- destroy-env.sh
+++ destroy-env.sh
@@ -2,27 +2,32 @@
 aws autoscaling update-auto-scaling-group --auto-scaling-group-name itmo544-hw4  --min-size 0 --max-size 0 --desired-capacity 0
 echo "Reducing outscaling groups"
 
+#Grab instances ids
 ID=$(aws ec2 describe-instances | awk {'print $8'}| grep 'i-0*')
-aws elb deregister-instances-from-load-balancer --load-balancer-name itmo544-dr --instances $ID
-echo "Detach load balancer"
 
+#Deregister instances from the load balancer
+aws elb deregister-instances-from-load-balancer --load-balancer-name itmo544-dr --instances $ID
+echo "Deregister load balancer"
+
+#Detach from autoscaling group
 aws autoscaling detach-load-balancers --load-balancer-names itmo544-dr --auto-scaling-group-name itmo544-hw4
 echo "Detach load balancer from autoscaling group"
 
+#Delete Load balancer
 aws elb delete-load-balancer --load-balancer-name itmo544-dr
 echo "Delete load balancer"
 
 
-
+#Terminate all instances and wait until terminated
 aws ec2 terminate-instances --instance-ids $ID
 echo "Waiting..."
 aws ec2 wait instance-terminated --instance-ids $ID
 
-
+#Delete autoscaling group
 aws autoscaling delete-auto-scaling-group --auto-scaling-group-name itmo544-hw4
 echo "Deleting autoscaling group"
 
-
+#delete launch config
 aws autoscaling delete-launch-configuration --launch-configuration-name itmo544-dr-hw4
 echo "Deleteing Launch Configuration"
 
